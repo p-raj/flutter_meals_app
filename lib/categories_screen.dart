8feace7b2da4dfa116a41221663e163132022d53
Peeyush <praj@teramatrix.in>
2019-10-09 15:49:45 +0530

    setting up categories list and data in a categories grid

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 
-import './models/category.dart';
+import './dummy_data.dart';
+import './category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       child: GridView(
-        children: <Widget>[
-
-        ],
+        children: DUMMY_CATEGORIES.map(
+          (catData) => CategoryItem(
+            catData.title, 
+            catData.color
+          )
+        ).toList(),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
           childAspectRatio: 3/2,
@@ -20,56 +24,3 @@ class CategoriesScreen extends StatelessWidget {
     );
   }
 }
-
-const DUMMY_CATEGORIES = const [
-  Category(
-    id: 'c1',
-    title: 'Italian',
-    color: Colors.purple,
-  ),
-  Category(
-    id: 'c2',
-    title: 'Quick & Easy',
-    color: Colors.red,
-  ),
-  Category(
-    id: 'c3',
-    title: 'Hamburgers',
-    color: Colors.orange,
-  ),
-  Category(
-    id: 'c4',
-    title: 'German',
-    color: Colors.amber,
-  ),
-  Category(
-    id: 'c5',
-    title: 'Light & Lovely',
-    color: Colors.blue,
-  ),
-  Category(
-    id: 'c6',
-    title: 'Exotic',
-    color: Colors.green,
-  ),
-  Category(
-    id: 'c7',
-    title: 'Breakfast',
-    color: Colors.lightBlue,
-  ),
-  Category(
-    id: 'c8',
-    title: 'Asian',
-    color: Colors.lightGreen,
-  ),
-  Category(
-    id: 'c9',
-    title: 'French',
-    color: Colors.pink,
-  ),
-  Category(
-    id: 'c10',
-    title: 'Summer',
-    color: Colors.teal,
-  ),
-];
