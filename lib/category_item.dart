f1b47b8a59d17856ec3d297339f3dc49005719a8
Peeyush <praj@teramatrix.in>
2019-10-09 16:44:52 +0530

    Navigator: to switch screens

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,27 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_meals_app/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
-
   final String title;
   final Color color;
 
   CategoryItem(this.title, this.color);
 
+  void selectCategory(BuildContext bc) {
+    Navigator.of(bc).push(
+      MaterialPageRoute(
+        builder: (_){
+          return CategoryMealsScreen();
+        },
+        fullscreenDialog: true,
+      )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(title),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            color.withOpacity(0.7),
-            color.withOpacity(1),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight
-        ),
-        borderRadius: BorderRadius.circular(15)
+    return InkWell(
+      onTap: () => selectCategory(context),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(title),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+              color.withOpacity(0.7),
+              color.withOpacity(1),
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+            borderRadius: BorderRadius.circular(15)),
       ),
     );
   }
