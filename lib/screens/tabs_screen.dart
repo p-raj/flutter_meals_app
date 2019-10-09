a0175fb0091796640b3331d5f991f383d9270199
Peeyush <praj@teramatrix.in>
2019-10-09 19:30:13 +0530

    bottom tabs

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -9,33 +9,47 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+
+  List<Widget> _pages = [
+    CategoriesScreen(),
+    FavoritesScreen()
+  ];
+
+  int _selectPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('data'),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categories',
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: 'Favorites',
-              )
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            CategoriesScreen(),
-            FavoritesScreen(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('data'),
+        bottom: TabBar(
+          tabs: <Widget>[
+            Tab(
+              icon: Icon(Icons.category),
+              text: 'Categories',
+            ),
+            Tab(
+              icon: Icon(Icons.star),
+              text: 'Favorites',
+            )
           ],
         ),
       ),
+      body: _pages[_selectPageIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        items: [
+        BottomNavigationBarItem(
+            icon: Icon(Icons.category), title: Text('Category')),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.star), title: Text('Favorites'))
+      ]),
     );
   }
 }
